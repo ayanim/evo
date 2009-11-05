e42727f71ad29ade990a8daddd27cb343f59c05f
visionmedia <tj@vision-media.ca>
2009-11-04 18:39:20 -0800

    More system route specs

--- lib/evo/packages/system/spec/system_routes_spec.rb
+++ lib/evo/packages/system/spec/system_routes_spec.rb
@@ -1,11 +1,16 @@
 
 describe "system" do
-  describe "get /:pacage/public/*" do
+  describe "get /:package/public/*" do
     it "should transfer a file when it is present" do
-      get '/public/foo/style.css'
+      get '/foo/public/style.css'
       last_response.should be_ok
       last_response.should have_content_type('text/css')
       last_response.body.should match('super cool')
+    end
+    
+    it "should respond with 404 when not present" do
+      get '/foo/public/something'
+      last_response.should_not be_ok
     end
   end
 end
