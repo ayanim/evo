8d69f03a97f081e54626ccde48f3ce991755d09c
visionmedia <tj@vision-media.ca>
2009-11-04 16:40:48 -0800

    Specs for Evo.package_load_path

--- spec/unit/evo_spec.rb
+++ spec/unit/evo_spec.rb
@@ -5,4 +5,14 @@ describe Evo do
   it "should be Sinatra::Application" do
     Evo.should == Sinatra::Application
   end
+  
+  describe "#package_load_path" do
+    it "should start with the application" do
+      Evo.load_path.first.should include('app/application/packages')
+    end
+    
+    it "should end with core" do
+      Evo.load_path.last.should include('evo/packages')
+    end
+  end
 end
