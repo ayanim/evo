915b16a2d3bdfef3db623d2dc5d81eda181032eb
visionmedia <tj@vision-media.ca>
2009-11-05 12:32:42 -0800

    More #message specs

--- lib/evo/packages/system/spec/system_helpers_spec.rb
+++ lib/evo/packages/system/spec/system_helpers_spec.rb
@@ -14,6 +14,8 @@ describe "system" do
       get '/user'
       last_response.should be_ok
       last_response.body.should include('<li>User deleted</li>')
+      get '/user'
+      last_response.body.should_not include('<li>User deleted</li>')
     end
   end
 end
