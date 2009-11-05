3f7636c402cf991138ab118526ef8a90e45a9790
visionmedia <tj@vision-media.ca>
2009-11-04 17:56:10 -0800

    No longer using root package rb files

--- lib/evo/package.rb
+++ lib/evo/package.rb
@@ -13,24 +13,19 @@ class Evo
     attr_reader :path
     
     ##
-    # Package initialization file path.
-    
-    attr_reader :file
-    
-    ##
     # Initialize with _path_ to the package.
     
     def initialize path
       @path = path
       @name = File.basename(path).to_sym
-      @file = path / name + '.rb'
     end
     
     ##
     # Load the package.
     
     def load
-      require file
+      load_directory :models
+      load_directory :routes
       self
     end
     
