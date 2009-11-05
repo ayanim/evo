6b504ee51c42cc36d7f647fce947433d25e81eb3
visionmedia <tj@vision-media.ca>
2009-11-04 16:04:00 -0800

    Helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,35 @@
 $:.unshift File.dirname(__FILE__) + '/../lib'
 require 'rubygems'
 require 'evo'
+
+# Sinatra
+
+Sinatra::Base.set :environment, :test
+Sinatra::Base.set :run, false
+Sinatra::Base.set :raise_errors, true
+Sinatra::Base.set :logging, false
+
+# DataMapper
+
+DataMapper.setup :default, 'sqlite3::memory:'
+
+# Spec configuration
+
+Spec::Runner.configure do |c|
+  
+  ##
+  # Reset database
+  
+  c.before :each do
+    DataMapper.auto_migrate!
+  end
+  
+  ##
+  # Helpers
+  
+  c.include do
+    def app
+      Sinatra::Application
+    end
+  end
+end
