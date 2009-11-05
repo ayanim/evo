fce3d8cbf24aa8764d51e5578fc6a44b83a27792
visionmedia <tj@vision-media.ca>
2009-11-04 17:25:07 -0800

    Docs

--- lib/evo/package.rb
+++ lib/evo/package.rb
@@ -1,15 +1,33 @@
 
 class Evo
   class Package
+    
+    ##
+    # Package name symbol.
+    
     attr_reader :name
+    
+    ##
+    # Package directory path.
+    
     attr_reader :path
+    
+    ##
+    # Package initialization file path.
+    
     attr_reader :file
+    
+    ##
+    # Initialize with _path_ to the package.
     
     def initialize path
       @path = path
       @name = File.basename(path).to_sym
       @file = path / name + '.rb'
     end
+    
+    ##
+    # Load the package.
     
     def load!
       load file
