2b770daf144b0ff4b410a409015f450a412d38a8
visionmedia <tj@vision-media.ca>
2009-11-10 14:44:34 -0800

    Added Evo::Block

--- lib/evo/packages/block/lib/block.rb
+++ lib/evo/packages/block/lib/block.rb
@@ -1,10 +1,24 @@
 
 class Evo
   class Block
+    
+    ##
+    # Weight.
+    
     attr_accessor :weight
     
-    def initialize
-      @weight = 0
+    ##
+    # Block contents.
+    
+    attr_accessor :contents
+    alias :to_html :contents
+    alias :to_s :contents
+    
+    ##
+    # Initialize block with _contents_.
+    
+    def initialize contents = nil
+      @contents, @weight = contents, 0
     end
   end
 end
--- lib/evo/packages/block/spec/block_spec.rb
+++ lib/evo/packages/block/spec/block_spec.rb
@@ -1,5 +1,11 @@
 
 describe Evo::Block do
+  describe "#initialize" do
+    it "should accept contents" do
+      Evo::Block.new('test').contents.should == 'test'
+    end
+  end
+  
   describe "#weight" do
     it "should default to 0" do
       Evo::Block.new.weight.should == 0
@@ -9,6 +15,22 @@ describe Evo::Block do
       block = Evo::Block.new
       block.weight = 4
       block.weight.should == 4
+    end
+  end
+  
+  describe "#contents" do
+    it "should be assignable" do
+      block = Evo::Block.new
+      block.contents = 'test'
+      block.contents.should == 'test'
+    end
+    
+    it "should be aliased as #to_html" do
+      Evo::Block.new('test').to_html.should == 'test'
+    end
+    
+    it "should be aliased as #to_s" do
+      Evo::Block.new('test').to_s.should == 'test'
     end
   end
 end
