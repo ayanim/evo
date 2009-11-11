fe5231bfdeccb0f9adb7ebbce1e196cb59c51da9
visionmedia <tj@vision-media.ca>
2009-11-11 13:46:37 -0800

    Started user authentication

--- lib/evo/packages/user/routes/user_routes.rb
+++ lib/evo/packages/user/routes/user_routes.rb
@@ -1,7 +1,52 @@
 
+before do
+  session[:id] ||= token
+  sess = Session.get(session[:id]) || Session.create(:id => session[:id], :hostname => env['REMOTE_ADDR'])
+  sess.last_request_at = DateTime.now
+  sess.user = User.anonymous unless sess.user
+  sess.user = User.current = if sess.user
+      if sess.user.blocked?
+        regenerate_session
+        messages.error 'Your account is blocked'
+        User.anonymous
+      else
+        sess.user
+      end
+    else
+      User.anonymous
+    end
+  sess.save
+end
+
 ##
 # Display user login form.
 
 get '/login' do
   render :login
+end
+
+##
+# Attempt to authenticate a user.
+
+post '/login' do
+  regenerate_session
+  if user = User.authenticate(params[:username], params[:password])
+    user.session = Session.create :id => session[:id], :hostname => env['REMOTE_ADDR']
+    user.last_login_at = DateTime.now
+    if user.session.save && user.save
+      messages.info 'Login successful' unless user.blocked?
+    end
+  else
+    messages.error 'Invalid username / password combination'
+  end
+  render :login
+end
+
+##
+# Logout the current user.
+
+get '/logout' do
+  regenerate_session
+  messages.info 'Logout successful'
+  redirect '/login'
 end
--- lib/evo/packages/user/spec/user_routes_spec.rb
+++ lib/evo/packages/user/spec/user_routes_spec.rb
@@ -2,14 +2,18 @@
 describe "user" do
   before(:each) { Evo.seed }
   
+  def last_response_should_show_a_login_form
+    last_response.body.should include("name='username'")
+    last_response.body.should include("name='password'")
+    last_response.body.should include("name='op'")
+    last_response.body.should include("value='Login'")
+  end
+  
   describe "get /login" do
     it "should display a login form" do
       get '/login'
       last_response.should be_ok
-      last_response.body.should include("name='username'")
-      last_response.body.should include("name='password'")
-      last_response.body.should include("name='op'")
-      last_response.body.should include("value='Login'")
+      last_response_should_show_a_login_form
     end
   end
   
@@ -19,6 +23,8 @@ describe "user" do
         it "should authenticate the user" do
           post '/login', :username => 'admin', :password => 'password'
           last_response.should be_ok
+          last_response_should_show_a_login_form
+          get '/login'
           User.current.name.should == 'admin'
         end
       end
@@ -27,7 +33,7 @@ describe "user" do
         it "should display error messages" do
           post '/login', :username => 'admin', :password => 'invalid password'
           last_response.should be_ok
-          User.current.should == User.anonymous
+          last_response_should_show_a_login_form
         end
       end
     end
@@ -38,6 +44,8 @@ describe "user" do
           User.current = User.first
           post '/login', :username => 'admin', :password => 'invalid password'
           last_response.should be_ok
+          last_response_should_show_a_login_form
+          get '/login'
           User.current.should == User.anonymous
         end
       end
