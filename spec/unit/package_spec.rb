24661b2d20d2ff83f549fcb75fc43cb8c4a73362
visionmedia <tj@vision-media.ca>
2009-11-23 14:39:51 -0800

    Fixed some package view lookup specs

--- spec/unit/package_spec.rb
+++ spec/unit/package_spec.rb
@@ -22,20 +22,9 @@ describe Evo::Package do
     end
     
     it "should check packages in the load path first" do
-      @jobqueue.paths_to_view(:job).length.should == 2
-      @jobqueue.paths_to_view(:job)[0].should include('foo/views/jobqueue/job.haml')
-      @jobqueue.paths_to_view(:job)[1].should include('jobqueue/views/job.haml')
-    end
-    
-    it "should check packages in the load path first disregarding engine suffix" do
       @jobqueue.paths_to_view(:jobs).length.should == 2
       @jobqueue.paths_to_view(:jobs)[0].should include('foo/views/jobqueue/jobs.erb')
       @jobqueue.paths_to_view(:jobs)[1].should include('jobqueue/views/jobs.haml')
-    end
-    
-    it "should result to returning the original view path" do
-      @jobqueue.paths_to_view(:worker).length.should == 1
-      @jobqueue.paths_to_view(:worker).first.should include('jobqueue/views/worker.haml')
     end
   end
   
@@ -43,6 +32,7 @@ describe Evo::Package do
     it "should return the first available path" do
       @package.path_to_view(:bar).should include('bar.erb')
       @jobqueue.path_to_view(:jobs).should include('foo/views/jobqueue/jobs.erb')
+      @jobqueue.path_to_view(:foo).should include('foo/views/jobqueue/foo.haml')
     end
   end
   
