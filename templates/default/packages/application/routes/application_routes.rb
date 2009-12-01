ffe49c862e0731b8fa714bb2f4afa82d88b1d4f5
visionmedia <tj@vision-media.ca>
2009-12-01 10:26:57 -0800

    More template data

--- templates/default/packages/application/routes/application_routes.rb
+++ templates/default/packages/application/routes/application_routes.rb
@@ -1,8 +1,10 @@
 
 before do
   javascripts.add '/application/javascripts/application.js'
+  @menu.add Menu::Item.new('Hello', '/hello', :when => 'view example')
 end
 
 get '/hello' do
+  require_permission_to 'view example'
   render :hello
 end
